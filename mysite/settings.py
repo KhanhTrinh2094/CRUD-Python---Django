0b1e68daeb46c07e2ddd8441570347714394edad
TRINH.NCK <trinh.nck@gmail.com>
2017-03-05 00:23:13 +0700

    Startapp file uploader

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'file_uploader.apps.FileUploaderConfig',
     'user_auth.apps.UserAuthConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
